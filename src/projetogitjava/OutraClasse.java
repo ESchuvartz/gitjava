9380776b1b579b2116ecd587eb8a529a98f0dfd6
eduardo.151294 <eduardo.151294@192.168.3.70>
2014-11-05 19:28:22 -0200

    Adicionando OutraClasse

---
+++ src/projetogitjava/OutraClasse.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetogitjava;
+
+/**
+ *
+ * @author eduardo.151294
+ */
+public class OutraClasse {
+    
+}
